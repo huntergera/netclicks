539774c14706c1f739c6d4d798dd51ad7c6a6559
huntergera <huntergera91@gmail.com>
2020-05-25 22:20:27 +0300

    first commit

---
+++ script/main.js
@@ -0,0 +1,31 @@
+// menu
+
+const leftMenu = document.querySelector('.left-menu');
+const hamburger = document.querySelector('.hamburger');
+
+// open/close menu
+
+hamburger.addEventListener('click', () => {
+    leftMenu.classList.toggle('openMenu');
+    hamburger.classList.toggle('open');
+});
+
+document.body.addEventListener('click', (event) => {
+    
+    if (!event.target.closest('.left-menu')) {
+       console.log('клик вне меню')
+       leftMenu.classList.remove('openMenu');
+       hamburger.classList.remove('open');
+    };
+
+});
+
+leftMenu.addEventListener('click', event => {
+    const target = event.target;
+    const dropdown = target.closest('.dropdown');
+    if(dropdown) {
+        dropdown.classList.toggle('active');
+        leftMenu.classList.add('openMenu')
+        hamburger.classList.add('open')
+    }
+});
